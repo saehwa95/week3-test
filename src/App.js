ee28763e98d19b4475ca7220eb886730a499a28c
Saehwa95 <tpghk1002@naver.com>
2022-12-01 11:49:02 +0900

    주특기입문 시험

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, content: "리액트를 배워봅시다." },
+  ]);
+
+  const [content, setContent] = useState("");
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    const newTodo = {
+      id: todos.length + 1,
+      content,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const inputChangeHandler = (e) => {
+    setContent(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrapper">
+        <div className="write-box">
+          <form onSubmit={addTodo}>
+            <input type="text" onChange={inputChangeHandler} />
+            <button type="submit">추가하기</button>
+          </form>
+        </div>
+        <h1>Todo List</h1>
+        <div className="todo-box">
+            <div className="todo-text">
+              <span>{todos.content}</span>
+            </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
